28e5231a6092c49e62718f8ab749ccdcdf435ef2
Hien Pham <hpham474@gmail.com>
2024-07-16 21:03:06 -0500

    create node class and add functions parameters to linkedlist

---
+++ LinkedList.mjs
@@ -0,0 +1,15 @@
+class LinkedList {
+    list;
+    constructor() {}
+    prepend() {}
+    size() {}
+    head() {}
+    tail() {}
+    at(index) {}
+    pop() {}
+    contains(value) {}
+    find(value) {}
+    insertAt(value, index) {}
+    removeAt(index) {}
+    toString() {}
+}
---
+++ Node.mjs
@@ -0,0 +1,20 @@
+class Node {
+    value;
+    nextNode;
+    constructor() {
+        this.value = null;
+        this.nextNode = null;
+    }
+    get value () {
+        return this.value;
+    }
+    get nextNode () {
+        return this.nextNode;
+    }
+    set value (val) {
+        this.value = val;
+    }
+    set nextNode (node) {
+        this.nextNode = node;
+    }
+}
